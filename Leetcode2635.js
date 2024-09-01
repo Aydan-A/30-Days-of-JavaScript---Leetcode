cf884a21e02748d65de076dcaaefb072bdf2081a
Aydan Abbaslı <130776228+Aydan-A@users.noreply.github.com>
2024-08-31 21:49:09 -0400

    Add files via upload

---
+++ Leetcode2635.js
@@ -0,0 +1,21 @@
+
+// The map function takes an array arr and a function fn as input.
+var map = function (arr, fn) {
+  let returnedArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    // For each element, the fn function is called
+      returnedArray.push(fn(arr[i], i));  
+  }
+  return returnedArray;
+};
+
+fn = function plusI(n, i) {
+  return n + i;
+};
+arr = [1, 2, 3];
+const newArray = map(arr, plusone);
+console.log(newArray);
+
+
+ 
