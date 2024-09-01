c3e0fd3501c8dc5538d0083ab4f988832b807f3a
Aydan Abbaslı <130776228+Aydan-A@users.noreply.github.com>
2024-08-31 21:50:30 -0400

    Comments are added

--- tobeornottobe.js
+++ tobeornottobe.js
@@ -1,3 +1,14 @@
+
+// This code is responsible for returning an object containing the toBe and notToBe functions. 
+
+// I want to emphasize this part that many people forget.
+
+// return { toBe: toBe,
+//        notToBe: notToBe, };
+
+// The expect function returns an object with two properties: toBe and notToBe. (with two key-value pairs.)
+// There are people who make mistakes especially in this part. They cannot write a return to the main function.
+
 var expect = function (val) {
     function toBe (val1) {
         if (val === val1) {
